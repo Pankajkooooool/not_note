8f6f60add3278c690e34852a514861f345ed560b
pankajkooooool <pankajkooooool6@gmail.com>
2024-12-16 19:41:59 +0530

    Changing cors settings

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,11 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: "*", 
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+}));
 
 /* ROUTES */
 app.use("/auth", authRoutes);
